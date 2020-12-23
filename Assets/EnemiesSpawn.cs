33f9a2d1e290df599d0a591007c7aff4f37ed237
Jonathan CONTE <etud@contejonathan.com>
2020-12-23 16:57:41 +0100

    multiple wolf spawn

---
+++ Assets/EnemiesSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesSpawn : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public int minxPos = 552;
+    public int maxxPos = 583;
+    public int minzPos = 37;
+    public int maxzPos = 53;
+    public int yPos = 52;
+    public int nbOfEnemies = 10;
+    public float timeToWaitBetweenEachEnemyCreation = 0.1f;
+
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        int enemyCount = 0;
+        while (enemyCount < nbOfEnemies)
+        {
+            var xPos = Random.Range(minxPos, maxxPos);
+            var zPos = Random.Range(minzPos, maxzPos);
+            Instantiate(enemy, new Vector3(xPos, yPos, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(timeToWaitBetweenEachEnemyCreation);
+            enemyCount += 1;
+        }
+    }
+
+}
