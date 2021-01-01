6656c7470fc4026b105a2d127310d21b997f7b17
Jonathan CONTE <etud@contejonathan.com>
2021-01-01 20:08:09 +0100

    clean + zqsd

--- Assets/Game/Scripts/Player/animationPlayerController.cs
+++ Assets/Game/Scripts/Player/animationPlayerController.cs
@@ -18,14 +18,6 @@ public class animationPlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);*/
-
-
         if(Input.GetButtonDown("Jump"))
         {
             anim.SetTrigger("isjumping");
