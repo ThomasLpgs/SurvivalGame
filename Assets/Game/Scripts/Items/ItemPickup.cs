4a20945722aa94bf8d76ea95a5b3291977516d6d
Jonathan CONTE <etud@contejonathan.com>
2021-01-01 17:14:05 +0100

    working with meat drop

--- Assets/Game/Scripts/Items/ItemPickup.cs
+++ Assets/Game/Scripts/Items/ItemPickup.cs
@@ -5,12 +5,34 @@ public class ItemPickup : MonoBehaviour
     public Player player;
     public GameObject myPrefab;
 
+    private float helperDeadTime = 0.0f;
+    private float deadPeriod = 1.0f;
+    private float nextActionTimeDead = 1.0f;
+    private bool canPickup;
+
+    private void Update()
+    {
+        if (canPickup)
+            return;
+
+        helperDeadTime += Time.deltaTime;
+        if (helperDeadTime > nextActionTimeDead)
+        {
+            nextActionTimeDead += deadPeriod;
+            // execute block of code here
+            canPickup = true;
+        }
+    }
+
     private void Awake()
     {
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.name != "Player" || !canPickup)
+            return;
+
         player.addHunger(20);
         Destroy(gameObject);
     }
--- Assets/Game/Scripts/Player/Player.cs
+++ Assets/Game/Scripts/Player/Player.cs
@@ -83,6 +83,8 @@ public class Player : MonoBehaviour
             currentHunger = 100;
         else
             currentHunger += p_addHunger;
+
+		hungerBar.SetHunger(currentHunger);
     }
 
 }
